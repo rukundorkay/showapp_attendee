26ecc3c98ade585615dbbd035c90e26c49cf8651
kabundege <christophekwizera1@gmail.com>
2022-03-22 08:59:07 +0200

    removes RNvector icon in search for an alternative for vector icons

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,17 +1,16 @@
-import { StatusBar, Text, View } from 'react-native'
-import React, { useContext } from 'react'
-import { styles } from './HomeScreen.styles'
+import { SafeAreaView, StatusBar, Text } from 'react-native'
+import React from 'react'
 import { colors } from '../../constants'
-import { StoreContext } from '../../context'
+import { styles } from './HomeScreen.styles'
 import { useContextMode } from '../../context/useContext'
 
 const HomeScreen = () => {
   const { authInfo } = useContextMode()
   return (
-    <View>
-      <Text style={styles.title}>{ authInfo?.name }</Text>
+    <SafeAreaView>
       <StatusBar backgroundColor={colors.primary} />
-    </View>
+      <Text style={styles.title}>{ authInfo?.name }</Text>
+    </SafeAreaView>
   )
 }
 
