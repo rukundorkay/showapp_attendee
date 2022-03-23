ba009beef3d5339e1e997a54d4083515a83bac4c
Manzi <asifiwemanzi@gmail.com>
2022-03-23 23:56:48 +0200

    add dateformat and make api requests

--- src/screens/Home/EventCard.tsx
+++ src/screens/Home/EventCard.tsx
@@ -1,20 +1,35 @@
 import React, {useEffect} from 'react';
 import {View, Image, Text, Pressable} from 'react-native';
 import {styles} from './HomeScreen.styles';
+import {formatDate} from '../../utils/dateFormat';
+
+let pic = Math.floor(Math.random() * 5);
+console.log(pic);
+const pics = [
+  require('../../assets/images/1.png'),
+  require('../../assets/images/2.png'),
+  require('../../assets/images/3.png'),
+  require('../../assets/images/4.png'),
+];
 
 const EventCard = ({navigation, event}: {navigation: any; event: any}) => {
+  let item = event.item;
   return (
     <Pressable
       style={styles.eventCardWrapper}
-      onPress={() => navigation.navigate('concert', {id: event.id})}>
+      onPress={() =>
+        navigation.navigate('concert', {concert: item, img: event.index})
+      }>
       <Image
         style={styles.eventPoster}
-        source={require('../../assets/images/wall.png')}
+        source={pics[event.index]}
         resizeMode="cover"
       />
       <View style={styles.eventInfoContainer}>
-        <Text style={styles.eventTitle}>{event.title}</Text>
-        <Text style={styles.eventStartDate}>{event.date}</Text>
+        <Text style={styles.eventTitle}>{event.item.title}</Text>
+        <Text style={styles.eventStartDate}>
+          {formatDate(event.item.startDate)}
+        </Text>
       </View>
     </Pressable>
   );
