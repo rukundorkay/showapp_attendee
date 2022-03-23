2db8a1ae88f9799d4673615b58c97a3d06190845
Manzi <asifiwemanzi@gmail.com>
2022-03-23 10:14:33 +0200

    configure routes

--- src/navigattion/index.tsx
+++ src/navigattion/index.tsx
@@ -3,7 +3,7 @@ import HomeScreen from '../screens/Home/HomeScreen';
 import {RootStackParamList} from '../../types';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import ProfileScreen from '../screens/Profile/';
+import ProfileScreen from '../screens/Profile';
 import BuyTicketScreen from '../screens/BuyTicketScreen/BuyTicketScreen';
 import LoginScreen from '../screens/Login';
 import InterestScreen from '../screens/Interest';
@@ -18,9 +18,9 @@ const options = {headerShown: false};
 
 export default () => (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName="BuyTicket" screenOptions={options}>
+    <Stack.Navigator initialRouteName="home" screenOptions={options}>
       <Stack.Screen name="home" component={HomeScreen} />
-      <Stack.Screen name="profile" component={ProfileScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="BuyTicket" component={BuyTicketScreen} />
       <Stack.Screen name="login" component={LoginScreen} />
       <Stack.Screen name="forgotPassword" component={ForgotPassword} />
